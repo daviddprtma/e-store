6b005695ce6d539bc8415706a6849f4d0aad804c
daviddprtma <davidchristianpratama@gmail.com>
2024-10-14 09:26:16 +0700

    e store app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { fetcher } from "./fetcher";
+import Category from "./components/Category";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetcher("/categories");
+      setCategories(data);
+    }
+    fetchData();
+  }, []);
+
+  const handleCategoryClick = (id) => {
+    fetch("http://localhost:3001/products?=" + id)
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  };
+
+  const renderCategory = () => {
+    return categories.map((result) => (
+      <Category
+        key={result.id}
+        id={result.id}
+        title={result.title}
+        onCategoryClick={() => handleCategoryClick(result.id)}
+      />
+    ));
+  };
+
+  const renderProducts = () => {
+    return products.map((result) => (
+      <div key={result.id}>
+        <p>{result.title}</p>
+      </div>
+    ));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>My Store</header>
+      <section>
+        <nav>{categories && renderCategory()}</nav>
+        <article>
+          <h1>Products </h1>
+          {products && renderProducts()}
+        </article>
+      </section>
+      <footer>My Footer</footer>
+    </>
   );
 }
 
